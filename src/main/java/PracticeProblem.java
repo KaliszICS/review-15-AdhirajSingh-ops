8a4de42eb1e04ff48c8c358856ce0d54d7528ad7
AdhirajSingh-ops <adhiraj.singh3@student.tdsb.on.ca>
2026-03-03 14:56:43 +0000

    done questions

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,26 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int goodString(String s) {
+    int n = s.length();
+    int minOperations = Integer.MAX_VALUE;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = n - 1; j > i; j--) {
+            if (s.charAt(i) == s.charAt(j)) {
+                int operations = i + (n - 1 - j);
+                minOperations = Math.min(minOperations, operations);
+            }
+        }
+    }
+
+    if (minOperations == Integer.MAX_VALUE) {
+        return -1;
+    }
+
+    return minOperations;
+}
 
 }
+
+
